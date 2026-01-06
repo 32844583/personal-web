d7cc6baa0e359b500740fc576d701ce3f2277b92
32844583 <youto201266@gmail.com>
2026-01-06 16:58:05 +0800

    更新blog

--- src/data/posts.ts
+++ src/data/posts.ts
@@ -1,17 +1,17 @@
 import { Post } from '@/types';
 
 export const posts: Post[] = [
-    {
-        slug: '論文 Graph Neural Networks: A Review of Methods and Applications',
-        title: '論文 Graph Neural Networks: A Review of Methods and Applications',
-        date: '2024-07-23',
-        summary: 'Graph Neural Network 的起源與原理',
-        tags: ['GNN'],
-        file: '/posts/graph-neural-network/index.md'
-    },
+    // {
+    //     slug: '論文 Graph Neural Networks: A Review of Methods and Applications',
+    //     title: '論文 Graph Neural Networks: A Review of Methods and Applications',
+    //     date: '2024-07-23',
+    //     summary: 'Graph Neural Network 的起源與原理',
+    //     tags: ['GNN'],
+    //     file: '/posts/graph-neural-network/index.md'
+    // },
     {
         slug: '論文 Stock Selection via Spatiotemporal Hypergraph Attention Network A Learning to Rank Approach',
-        title: 'Stock Selection via Spatiotemporal Hypergraph Attention Network A Learning to Rank Approach',
+        title: '論文 Stock Selection via Spatiotemporal Hypergraph Attention Network A Learning to Rank Approach',
         date: '2025-01-05',
         summary: '使用超圖卷積將行業維度應用於股票關聯預測',
         tags: ['STHAN-SR', 'Industry', 'Time step attention', 'Relationship attention'],
